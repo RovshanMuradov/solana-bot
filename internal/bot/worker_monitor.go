735324841252e027de5d037f72b9d6ac0418501c
Rovshan Muradov <63051606+RovshanMuradov@users.noreply.github.com>
2025-05-21 19:04:58 +0400

    small bug

--- internal/bot/worker_monitor.go
+++ internal/bot/worker_monitor.go
@@ -20,13 +20,15 @@ type SellFunc func(ctx context.Context, percent float64) error
 
 // MonitorWorker представляет рабочий процесс мониторинга
 type MonitorWorker struct {
-	ctx      context.Context
-	logger   *zap.Logger
-	task     *task.Task
-	dex      dex.DEX
-	session  *monitor.MonitoringSession
-	uiHandle *ui.Handler
-	sellFn   SellFunc
+	ctx             context.Context
+	logger          *zap.Logger
+	task            *task.Task
+	config          *task.Config
+	dex             dex.DEX
+	session         *monitor.MonitoringSession
+	uiHandle        *ui.Handler
+	sellFn          SellFunc
+	monitorInterval time.Duration
 }
 
 // NewMonitorWorker создает новый экземпляр рабочего процесса мониторинга
@@ -46,6 +48,8 @@ func NewMonitorWorker(
 		task:   t,
 		dex:    dexAdapter,
 		sellFn: sellFn,
+		// Store the monitor interval for later use
+		monitorInterval: monitorInterval,
 	}
 }
 
@@ -58,7 +62,7 @@ func (mw *MonitorWorker) Start() error {
 		InitialPrice:    0,
 		DEX:             mw.dex,
 		Logger:          mw.logger.Named("session"),
-		MonitorInterval: 1 * time.Second, // Используем стандартный интервал
+		MonitorInterval: mw.monitorInterval, // Используем интервал из конфига
 	}
 
 	// Создаем пользовательский интерфейс
